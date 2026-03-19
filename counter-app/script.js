29965f53203463ec611619f233fd5c8231c5c12f
divinelylimitless <divinelylimitless@gmail.com>
2026-03-19 12:49:55 +0000

    Add files via upload

--- counter-app/script.js
+++ counter-app/script.js
@@ -1,16 +1,29 @@
 let count = 0;
 
+function updateDisplay() {
+  const countElement = document.getElementById("count");
+  countElement.innerText = count;
+
+  if (count > 0) {
+    countElement.style.color = "green";
+  } else if (count < 0) {
+    countElement.style.color = "red";
+  } else {
+    countElement.style.color = "white";
+  }
+}
+
 function increaseCount() {
   count++;
-  document.getElementById("count").innerText = count;
+  updateDisplay();
 }
 
 function decreaseCount() {
   count--;
-  document.getElementById("count").innerText = count;
+  updateDisplay();
 }
 
 function resetCount() {
   count = 0;
-  document.getElementById("count").innerText = count;
+  updateDisplay();
 }
